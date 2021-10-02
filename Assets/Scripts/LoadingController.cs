1b200c6f42517ca0368916a7b20cb0f7683da8f5
andreasmarbun14 <andreasmarbun0@gmail.com>
2021-10-02 23:17:53 +0700

    init commit
    penambahan project cloud dengan firebase analytics

---
+++ Assets/Scripts/LoadingController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class LoadingController : MonoBehaviour
+
+{
+    [SerializeField] private Button _localButton;
+
+    [SerializeField] private Button _cloudButton;
+
+
+
+    private void Start()
+
+    {
+
+        _localButton.onClick.AddListener(() =>
+
+        {
+
+            SetButtonInteractable(false);
+
+            UserDataManager.LoadFromLocal();
+
+            SceneManager.LoadScene(1);
+
+        });
+
+
+
+        _cloudButton.onClick.AddListener(() =>
+
+        {
+
+            SetButtonInteractable(false);
+
+            StartCoroutine(UserDataManager.LoadFromCloud(() => SceneManager.LoadScene(1)));
+
+        });
+
+
+
+        // Button didisable agar mencegah tidak terjadinya spam klik ketika
+        //test : Andreas Project
+        // proses onclick pada button sedang berjalan
+
+    }
+
+
+
+    // Mendisable button agar tidak bisa ditekan
+    //Test : Andreas Project
+    private void SetButtonInteractable(bool interactable)
+
+    {
+
+        _localButton.interactable = interactable;
+
+        _cloudButton.interactable = interactable;
+
+    }
+
+}
